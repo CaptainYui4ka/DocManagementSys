00d93fe964cee0c5061f6d1de34b45b3a69a93ce
CaptainYuichka <yuichkacaptain@gmail.com>
2024-12-25 14:07:03 +0500

    1v dev

---
+++ src/components/addAgent.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import '../components/documentPages.css';
+
+const AddAgent = ({ fetchDocuments }) => {
+    const [agent, setAgent] = useState('');
+    const [documentName, setDocumentName] = useState('');
+    const [file, setFile] = useState(null); // Состояние для выбранного файла
+    const [message, setMessage] = useState("");
+
+    // Функция для обработки выбора файла
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!agent || !documentName || !file) {
+            setMessage("Все поля обязательны для заполнения");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('agent', agent);
+        formData.append('documentName', documentName);
+        formData.append('file', file);
+
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/documents`, {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                setMessage("Документ успешно загружен!");
+                setAgent("");
+                setDocumentName("");
+                setFile(null);
+                fetchDocuments(); // Обновляем список документов после загрузки
+            } else {
+                setMessage("Не удалось загрузить документ.");
+            }
+        } catch (error) {
+            console.error(error);
+            setMessage("Произошла ошибка при загрузке документа.");
+        }
+    };
+
+    return (
+        <div>
+            <section className="form-section">
+                <h2>Добавить новый документ</h2>
+                <form className="document-form" onSubmit={handleSubmit}>
+                    <label>
+                        Имя агента:
+                        <input
+                            type="text"
+                            value={agent}
+                            onChange={(e) => setAgent(e.target.value)}
+                            placeholder="Введите имя агента"
+                        />
+                    </label>
+                    <label>
+                        Название документа:
+                        <input
+                            type="text"
+                            value={documentName}
+                            onChange={(e) => setDocumentName(e.target.value)}
+                            placeholder="Введите название документа"
+                        />
+                    </label>
+                    <label className="file-upload">
+                        Выбрать файл:
+                        <input type="file" onChange={handleFileChange} />
+                    </label>
+                    <button type="submit">Загрузить</button>
+                </form>
+                {message && <p>{message}</p>}
+            </section>
+        </div>
+    );
+};
+
+export default AddAgent;
--- src/pages/documentPage.jsx
+++ src/pages/documentPage.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import '../components/documentPages.css'
+
+import AddAgent from '../components/addAgent';
 function DocumentForm() {
-  const [agent, setAgent] = useState('');
   const [documents, setDocuments] = useState([])
-  const [documentName, setDocumentName] = useState('');
-  const [file, setFile] = useState(null);
-  const [message, setMessage] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredDocuments, setFilteredDocuments] = useState(documents)
 
@@ -65,79 +63,13 @@ function DocumentForm() {
     }
   };
 
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!agent || !documentName || !file) {
-      setMessage("Все поля обязательны для заполнения");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append('agent', agent);
-    formData.append('documentName', documentName);
-    formData.append('file', file);
-
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/documents`, {
-        method: 'POST',
-        body: formData
-      });
-
-      if (response.ok) {
-        setMessage("Документ успешно загружен!");
-        setAgent("");
-        setDocumentName("");
-        setFile(null);
-        fetchDocuments(); // Обновляем список документов после загрузки
-      } else {
-        setMessage("Файл загрузился")
-      }
-    } catch (error) {
-      console.error(error);
-      setMessage("Ошибка!")
-    }
-  };
-
   return (
     <div className="document-page">
       <header className="header">
         <h1>Система управления документами</h1>
       </header>
       <main className="main-content">
-        <section className="form-section">
-          <h2>Добавить новый документ</h2>
-          <form className="document-form" onSubmit={handleSubmit}>
-            <label>
-              Имя агента:
-              <input
-                type="text"
-                value={agent}
-                onChange={(e) => setAgent(e.target.value)}
-                placeholder="Введите имя агента"
-              />
-            </label>
-            <label>
-              Название документа:
-              <input
-                type="text"
-                value={documentName}
-                onChange={(e) => setDocumentName(e.target.value)}
-                placeholder="Введите название документа"
-              />
-            </label>
-            <label className="file-upload">
-              Выбрать файл:
-              <input type="file" onChange={handleFileChange} />
-            </label>
-            <button type="submit">Загрузить</button>
-          </form>
-          {message && <p>{message}</p>}
-        </section>
-
+        <AddAgent fetchDocuments={fetchDocuments} />
         <section className="document-list">
           <h2>Загруженные документы</h2>
           <input
