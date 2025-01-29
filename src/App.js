564f9c031b158af5c1ccde88007fe515363d7f6e
CaptainYuichka <yuichkacaptain@gmail.com>
2025-01-29 09:26:24 +0500

    29.01.2025 v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 //import './App.css';
 import DocumentForm from './pages/documentPage';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
       <DocumentForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './footer.css'; // Импортируйте ваш CSS-файл
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="social-links">
+          <a href="https://vk.com/captainyuichka" className="social-link">
+            VK
+          </a>
+          <a href="https://t.me/CaptainYuichka" className="social-link">
+            Telegram
+          </a>
+          <a href="https://github.com/CaptainYui4ka/" className="social-link">
+            GitHub
+          </a>
+        </div>
+        <div className="footer-info">
+          <h6 className="footer-title">Система управления документами</h6>
+          <p className="footer-contact">
+            <span className="footer-icon">🏠</span> Россия, Уфа
+          </p>
+          <p className="footer-contact">
+            <span className="footer-icon">✉️</span> yuichkacaptain@gmail.com
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
